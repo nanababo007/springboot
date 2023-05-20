17e41fb135ae4aba7c6f74157f791e1c7c092870
DESKTOP-KIP66VH\kdh007 <nanababo007@naver.com>
2023-05-20 11:22:23 +0900

    스프링 부트 버전을 기존 3.1.0 에서, 3.0.7 로 바꿔서 다시 생성함.

---
+++ src/test/java/com/kdhppo/smplcms/SmplcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdhppo.smplcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmplcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
