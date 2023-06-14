3709fcc8e1afe5edcd924bbd553549a4ac7d1e71
DESKTOP-KIP66VH\kdh007 <nanababo007@naver.com>
2023-06-14 13:18:23 +0900

    시큐리티 로그인 사용자 정보 조회 함수 추가

--- src/main/java/com/kdhppo/smplcms/cmn/auth/SiteAuthInfo.java
+++ src/main/java/com/kdhppo/smplcms/cmn/auth/SiteAuthInfo.java
@@ -1,5 +1,14 @@
 package com.kdhppo.smplcms.cmn.auth;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.kdhppo.smplcms.svc.MembSvc;
 import com.kdhppo.smplcms.util.UtilClass;
 import com.kdhppo.smplcms.vo.memb.MembResVo;
 
@@ -8,6 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 /**
  * 인증된 사용자 정보.
  */
+@Component
 public class SiteAuthInfo {
 	/** 로그인한 회원 아이디 저장, 키 이름 */
 	private static final String cstLoginIdKeyName = "loginId";
@@ -15,6 +25,9 @@ public class SiteAuthInfo {
 	private static final String cstLoginInfoKeyName = "loginInfo";
 	/** 로그인한 회원 토큰, 키 이름 */
 	private static final String cstLoginTokenKeyName = "loginToken";
+
+	@Autowired
+	private MembSvc membSvc;
 
 	/**
 	 * 현재 로그인 여부 반환.
@@ -64,6 +77,43 @@ public class SiteAuthInfo {
 	 */
 	public static void setLoginUserToken(HttpServletRequest request, String token) {
 		request.getSession(true).setAttribute(cstLoginTokenKeyName, token);
+	}
+
+	/**
+	 * 시큐리티 사용자 정보 반환
+	 */
+	public static UserDetails getSecurityUserDetails() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserDetails userDetails = (UserDetails)principal;
+		//userDetails.getUsername();
+		//userDetails.getPassword();
+		return userDetails;
+	}
+
+	/**
+	 * 시큐리티 사용자 이름 반환
+	 */
+	public static String getSecurityUsername() {
+		UserDetails userDetails = getSecurityUserDetails();
+		return userDetails.getUsername();
+	}
+
+	/**
+	 * 시큐리티 사용자 아이디로 디비 조회한 전체 사용자 정보 반환
+	 *
+	 *@Autowired SiteAuthInfo siteAuthInfo;
+	 * MembResVo membResVo = siteAuthInfo.getSecurityUserInfoFromDB();
+	 * System.out.println("security userNm ::: "+membResVo.getMembNm());
+	 */
+	public MembResVo getSecurityUserInfoFromDB() {
+		//시큐리티에 저장된 회원 아이디 정보.
+		String userId = getSecurityUsername();
+
+		//로그인한 회원 정보 구하기.
+		Optional<MembResVo> menbInfoOpt = membSvc.getMembInfoById(userId);
+		MembResVo membInfo = menbInfoOpt.orElseThrow(() -> new UsernameNotFoundException(userId+" is not member."));
+
+		return membInfo;
 	}
 
 }
--- src/main/java/com/kdhppo/smplcms/ctl/MainCtl.java
+++ src/main/java/com/kdhppo/smplcms/ctl/MainCtl.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainCtl {
 
-    //메인 대시보드 페이지.
-    @GetMapping({"/", "/main/index.do"})
-    public String index(Model model) {
-        return "main/index";
-    }
+	//메인 대시보드 페이지.
+	@GetMapping({"/", "/main/index.do"})
+	public String index(Model model) {
+		return "main/index";
+	}
 
 }
