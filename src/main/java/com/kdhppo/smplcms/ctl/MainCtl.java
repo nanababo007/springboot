7b7637220e18c24b7021eb7d1fbe63c6195d260a
DESKTOP-KIP66VH\kdh007 <nanababo007@naver.com>
2023-06-23 15:03:04 +0900

    메인페이지 경로

--- src/main/java/com/kdhppo/smplcms/ctl/MainCtl.java
+++ src/main/java/com/kdhppo/smplcms/ctl/MainCtl.java
@@ -3,7 +3,6 @@ package com.kdhppo.smplcms.ctl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,9 +10,15 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class MainCtl {
 
-	//메인 대시보드 페이지.
-	@GetMapping({"/", "/main/index.do", "/main/dash.do"})
+	//메인 페이지.
+	@GetMapping({"/", "/main/index.do"})
 	public String index(Model model) {
+		return "redirect:/main/dash.do";
+	}
+
+	//메인 대시보드 페이지.
+	@GetMapping("/main/dash.do")
+	public String mainDash(Model model) {
 		return "main/index";
 	}
 
